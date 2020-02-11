36a17ac98dcf4bec452ccfea127dac55c87cadba
Mikolaj <mikolajbibro@gmail.com>
2020-02-11 20:21:58 +0100

    optimized imports

--- src/main/java/com/bibro/config/DockerConfig.java
+++ src/main/java/com/bibro/config/DockerConfig.java
@@ -1,13 +1,9 @@
 package com.bibro.config;
 
 import com.github.dockerjava.api.DockerClient;
-import com.github.dockerjava.api.model.Frame;
 import com.github.dockerjava.core.DockerClientBuilder;
-import com.github.dockerjava.core.command.LogContainerResultCallback;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class DockerConfig {
