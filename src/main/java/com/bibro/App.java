4c6bba89a3bbe3e0ec653b618fe1aabd238adf02
Mikolaj <mikolajbibro@gmail.com>
2020-02-18 18:03:09 +0100

    refactoring

--- src/main/java/com/bibro/App.java
+++ src/main/java/com/bibro/App.java
@@ -1,13 +1,26 @@
 package com.bibro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App  {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
 }
+
+
