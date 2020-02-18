911b34bb9145e23bc315342839a9021663652a38
Mikolaj <mikolajbibro@gmail.com>
2020-02-18 18:00:02 +0100

    added jwt authentication and security config

---
+++ src/main/java/com/bibro/config/JWTAuthorizationFilter.java
@@ -0,0 +1,63 @@
+package com.bibro.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.bibro.repository.UserRepository;
+import com.bibro.service.UserDetailsServiceImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.bibro.config.SecurityConstants.*;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final UserRepository userRepository;
+
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager, UserDetailsServiceImpl userDetailsService, UserRepository userRepository) {
+        super(authenticationManager);
+        this.userDetailsService = userDetailsService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader(HEADER_STRING);
+
+        System.out.println(header + "d11111sdssd");
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+        System.out.println(token + "dsdssd");
+        if (token != null) {
+            DecodedJWT decoded = JWT.require(Algorithm.HMAC256(SECRET.getBytes()))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""));
+            if (decoded != null) {
+                return new UsernamePasswordAuthenticationToken(decoded.getSubject(), null, new ArrayList<>());
+            }
+            return null;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/bibro/config/UserCredentials.java
@@ -0,0 +1,11 @@
+package com.bibro.config;
+
+import lombok.Data;
+
+@Data
+public class UserCredentials {
+    private String username;
+    private String password;
+
+
+}
