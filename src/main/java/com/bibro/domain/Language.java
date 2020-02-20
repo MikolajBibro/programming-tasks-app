d6ddcb0991fc46d1539284e250b0c13a49171272
Mikolaj <mikolajbibro@gmail.com>
2020-02-20 15:17:18 +0100

    new method to return all languages

--- src/main/java/com/bibro/domain/Language.java
+++ src/main/java/com/bibro/domain/Language.java
@@ -2,6 +2,10 @@ package com.bibro.domain;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public enum Language {
     C(".c", "C:\\Users\\Mikolaj\\Desktop\\new\\Dockerfile", "C:\\Users\\Mikolaj\\Desktop\\new\\"),
@@ -16,5 +20,11 @@ public enum Language {
         this.extension = extension;
         this.dockerfile = dockerfile;
         this.directoryPath = directoryPath;
+    }
+
+    public static List<String> getLanguages() {
+        return Arrays.stream(Language.values())
+                .map(Enum::name)
+                .collect(Collectors.toList());
     }
 }
